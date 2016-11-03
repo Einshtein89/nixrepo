6b1a6925a7b477a9a048eea947cb2dd9f0cd8c17
Evgen Makhnonosov <evgen.makhnonosov@.nixsolutions.com>
2016-11-03 17:39:36 +0200

    1

--- src/main/webapp/resources/js/slide_button.js
+++ src/main/webapp/resources/js/slide_button.js
@@ -2,7 +2,7 @@ var main = function() {
 
  $(".btn-slide").click(function(){
 	$(".container_1").slideToggle("slow");
-		$('.btn-slide').toggleClass("active"); return false;
+		$('.btn-slide').toggleClass("active");
 	});
 
 }
